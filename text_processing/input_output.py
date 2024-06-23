7e4b2d7189b36f4275f38f0e2fb579cc10ca5cf3
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-06-23 07:43:41 +0530

    removal of waste

--- text_processing/input_output.py
+++
@@ -1,9 +0,0 @@
-# Read input text from file
-def read_input_text(file_path):
-    with open(file_path, 'r', encoding='utf-8') as file:
-        text = file.read()
-    return text
-# Write the rephrased text to a new file
-def write_rephrased_text(rephrased_text, file_path):
-    with open(file_path, 'w', encoding='utf-8') as file:
-        file.write(rephrased_text)
--- text_processing/phrase_identification.py
+++
@@ -1,7 +0,0 @@
-# Identify complex phrases
-def identify_complex_phrases(words):
-    complex_phrases = []
-    for word in words:
-        if len(word) > 10:
-            complex_phrases.append(word)
-    return complex_phrases
