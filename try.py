5104ea7aa5d8bdcc6fc9d329f2f7f00c90f978c1
Siddhant Srivastava <ssiddhant1009@gmail.com>
2024-06-25 21:00:33 +0530

    Update try.py

--- try.py
+++ try.py
@@ -1,22 +1,22 @@
-# import openai
+# # import openai
 
-# openai.api_key = 'sk-proj-wUyed3Ao0Fvy9WVGwwaUT3BlbkFJqUZt1Jywu1so3wT73rMl'
+# # openai.api_key = 'sk-proj-wUyed3Ao0Fvy9WVGwwaUT3BlbkFJqUZt1Jywu1so3wT73rMl'
 
-# completion = openai.ChatCompletion.create(
-#   model="gpt-3.5-turbo",
-#   messages=[
-#     {"role": "system", "content": "You are a helpful assistant."},
-#     {"role": "user", "content": "Hello!"}
-#   ]
-# )
+# # completion = openai.ChatCompletion.create(
+# #   model="gpt-3.5-turbo",
+# #   messages=[
+# #     {"role": "system", "content": "You are a helpful assistant."},
+# #     {"role": "user", "content": "Hello!"}
+# #   ]
+# # )
 
-# print(completion.choices[0].message)
+# # print(completion.choices[0].message)
 
 
-# if needed, install and/or upgrade to the latest version of the OpenAI Python library
-%pip install --upgrade openai
-# import the OpenAI Python library for calling the OpenAI API
-from openai import OpenAI
-import os
+# # if needed, install and/or upgrade to the latest version of the OpenAI Python library
+# %pip install --upgrade openai
+# # import the OpenAI Python library for calling the OpenAI API
+# from openai import OpenAI
+# import os
 
-client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
+# client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY", "<your OpenAI API key if not set as env var>"))
